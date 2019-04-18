e3f4543934ecd78b7367cdf6bc2bc66b95e2df4a
zankeii <42954203+zankeii@users.noreply.github.com>
2019-04-18 18:50:18 -0400

    New components created
    Components created:
    -home
    -login
    -profile
    -conversation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
