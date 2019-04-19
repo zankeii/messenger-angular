d27b86005d4bcfbff1a18a893bc3b7f7e93a4e7f
zankeii <42954203+zankeii@users.noreply.github.com>
2019-04-18 20:18:06 -0400

    Directiva *ngFor
    Mostramos en el home.component.html con la directiva ngfor un arreglo de una lista de amigos estatica agregada al home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,20 +7,45 @@ import {User} from "../interfaces/user";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  friends: User[];
 
   constructor() {
-    let myUser: User = {
-      nick: 'Miguel',
-      subnick: 'zankeii',
-      age: 21,
-      email: 'm@gmail.com',
+    let usuario1: User = {
+      nick: 'Eduardo',
+      age: 24,
+      email: 'ed@aoe.aoe',
       friend: true,
       uid: 1
     };
-    let users: User[] = [
-      myUser,
-    ];
-    console.log(myUser);
+    let usuario2: User = {
+      nick: 'Freddy',
+      age: 28,
+      email: 'fred@aoe.aoe',
+      friend: true,
+      uid: 2
+    };
+    let usuario3: User = {
+      nick: 'Yuliana',
+      age: 18,
+      email: 'yuli@aoe.aoe',
+      friend: true,
+      uid: 3
+    };
+    let usuario4: User = {
+      nick: 'Ricardo',
+      age: 17,
+      email: 'rick@aoe.aoe',
+      friend: false,
+      uid: 4
+    };
+    let usuario5: User = {
+      nick: 'Marcos',
+      age: 30,
+      email: 'marcos@aoe.aoe',
+      friend: false,
+      uid: 5
+    };
+    this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
   }
 
   ngOnInit() {
