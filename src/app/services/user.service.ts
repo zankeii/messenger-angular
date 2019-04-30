0eec6532461f20ab2661ce72a962720d720ec074
zankeii <42954203+zankeii@users.noreply.github.com>
2019-04-29 20:05:11 -0400

    iconos de status
    Implementando un nuevo campo status en la interfaz usuario, mostrar logo status en el home.html

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,35 +13,40 @@ export class UserService {
       age: 24,
       email: 'ed@aoe.aoe',
       friend: true,
-      uid: 1
+      uid: 1,
+      status: 'online'
     };
     let usuario2: User = {
       nick: 'Freddy',
       age: 28,
       email: 'fred@aoe.aoe',
       friend: true,
-      uid: 2
+      uid: 2,
+      status: 'away'
     };
     let usuario3: User = {
       nick: 'Yuliana',
       age: 18,
       email: 'yuli@aoe.aoe',
       friend: true,
-      uid: 3
+      uid: 3,
+      status: 'busy'
     };
     let usuario4: User = {
       nick: 'Ricardo',
       age: 17,
       email: 'rick@aoe.aoe',
       friend: false,
-      uid: 4
+      uid: 4,
+      status: 'offline'
     };
     let usuario5: User = {
       nick: 'Marcos',
       age: 30,
       email: 'marcos@aoe.aoe',
       friend: false,
-      uid: 5
+      uid: 5,
+      status: 'appear_offline'
     };
     this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
 
