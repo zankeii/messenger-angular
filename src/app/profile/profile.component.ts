7a6452c303c0f552b37d7e97ee21701463134ed6
zankeii <42954203+zankeii@users.noreply.github.com>
2019-05-03 14:00:01 -0400

    Guardando las imágenes de perfil en nuestra base de datos de firebase
    Para incluir la funcionalidad de agregar foto a nuestro perfil, usaremos el servicio Firebase Storage de Firebase, con AngularFireStorage.
    
    El servicio FirebaseStorage funciona como un repositorio de recursos estáticos (organizado en carpetas) y se implementa en Angular a través del módulo AngularFireStorageModule, que debemos incluir en app.modules.ts
    
    Finalmente, subiremos la imagen recortada a FirebaseStorage en formato base64, que será convertida por el storage a formato binario. Guardaremos luego en la base de datos, la referencia a la url de esa imagen que obtenemos mediante el método getDownloadURL().

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { User } from '../interfaces/user';
 import { UserService } from '../services/user.service';
 import { AuthenticationService } from '../services/authentication.service'
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import { FirebaseStorage } from '@angular/fire';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-profile',
@@ -13,7 +15,10 @@ export class ProfileComponent implements OnInit {
   user: User;
   imageChangedEvent: any = '';
   croppedImage: any = '';
-  constructor(private userService: UserService, private authenticationService: AuthenticationService) {
+  picture: any;
+  constructor(private userService: UserService,
+              private authenticationService: AuthenticationService,
+              private firebaseStorage: AngularFireStorage) {
     this.authenticationService.getStatus().subscribe((status)=>{
       this.userService.getUserById(status.uid).valueChanges().subscribe((data: User)=>{
         this.user = data;
@@ -30,12 +35,31 @@ export class ProfileComponent implements OnInit {
   }
 
   saveSettings() {
-    this.userService.editUser(this.user).then(()=>{
-      alert('Changes Saved');
-    }).catch((error)=>{
-      alert('There was an error');
-      console.log(error);
-    })
+    if(this.croppedImage){
+      const currentPictureId = Date.now();
+      const pictures = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').putString(this.croppedImage, 'data_url');
+      pictures.then((result)=> {
+        this.picture = this.firebaseStorage.ref('pictures/' + currentPictureId + '.jpg').getDownloadURL();
+        this.picture.subscribe((p)=> {
+          this.userService.setAvatar(p, this.user.uid).then(()=> {
+            alert('Avatar subido correctamente');
+          }).catch((error)=> {
+            alert('Hubo un error al subir la imagen');
+            console.log(error);
+          })
+        })
+      }).catch((error)=> {
+        console.log(error);
+      });
+    }else{
+      this.userService.editUser(this.user).then(()=>{
+        alert('Changes Saved');
+      }).catch((error)=>{
+        alert('There was an error');
+        console.log(error);
+      });
+    }
+
   }
 
   fileChangeEvent(event: any): void {
