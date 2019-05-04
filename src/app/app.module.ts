891c395973b927187601ae5ebc0824e677471271
zankeii <42954203+zankeii@users.noreply.github.com>
2019-05-04 10:38:48 -0400

    Enviando una solicitud de amistad
    Esta característica nueva en nuestra app la crearemos a partir de un nuevo servicio que llamaremos requests. Debido a que almacenaremos la dirección de correo de usuarios, y ésta lleva puntos, tendremos que aplicar una transformación del string antes de guardarlo en la base de datos ya que Firebase no tolera el uso de puntos en los valores almacenados, esto es para evitar conflictos con el uso de puntos para indicar propiedades o atributos de objetos.
    
    Cada request tendrá, además de la dirección de correo, un atributo status que podrá tomar los valores de: aceptado, rechazado o pendiente.
    
    Implementaremos Bootstrap (a través de la librería ng-bootstrap) para el manejo de un diálogo de tipo modal para realizar la solicitud de amistades a otros usuarios.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { AngularFireDatabaseModule } from '@angular/fire/database'
 import { environment } from '../environments/environment';
 import { AuthenticationGuard } from './services/authentication.guard';
 import { ImageCropperModule } from 'ngx-image-cropper';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent, canActivate: [AuthenticationGuard]},
@@ -48,7 +49,8 @@ const appRoutes: Routes = [
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
-    ImageCropperModule
+    ImageCropperModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import {User} from "../interfaces/user";
 import {UserService} from "../services/user.service";
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { RequestsService } from '../services/requests.service';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +15,12 @@ export class HomeComponent implements OnInit {
   friends: User[];
   query: string = '';
   user: User;
+  closeResult: string;
+  friendEmail: string = '';
   constructor(private userService: UserService,
               private authenticationService: AuthenticationService,
-              private router: Router) {
+              private router: Router, private modalService: NgbModal,
+              private requestsService: RequestsService) {
     userService.getUsers().valueChanges().subscribe((data: User[]) => {
       this.friends = data;
     }, (error) => {
@@ -46,5 +51,24 @@ export class HomeComponent implements OnInit {
       console.log(error);
     });
   }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {}, (reason) => {});
+  }
+
+  sendRequest() {
+    const request = {
+      timestamp: Date.now(),
+      receiver_email: this.friendEmail,
+      sender: this.user.uid,
+      status: 'pending'
+    };
+    this.requestsService.createRequest(request).then(() => {
+      alert('Solicitud enviada');
+    }).catch((error) => {
+      alert('Ocurrio un error');
+      console.log(error);
+  });
+}
 
 }
